20dbd7355d398c566f0524423c75566b43e8fe2f
Marcel Rodriguez <BlasterNeko@gmail.com>
2024-05-15 22:48:38 -0700

    Revert

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="home flex min-h-screen flex-col items-center justify-between p-24 bg-white">
+      
     </main>
   );
 }
