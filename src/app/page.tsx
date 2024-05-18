73e9499f235398e309356b01ba06a5bebc91b840
Blasterphantom <blasterneko@gmail.com>
2024-05-18 16:28:52 -0700

    Working on main page poem

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,34 @@
 import Image from "next/image";
+import fs from 'fs/promises';
+import path from 'path';
+import { Poem } from "./interfaces/poem";
 
-export default function Home() {
+interface HomeProps {
+  poem: Poem | null;
+}
+
+async function fetchPoem(): Promise<Poem | null> {
+  const filePath = path.join(process.cwd(), 'src/app/Data/poems.json');
+  const jsonData = await fs.readFile(filePath, 'utf-8');
+  const poems: Poem[] = JSON.parse(jsonData);
+  return poems.length > 0 ? poems[0] : null;
+}
+
+
+export default async function Home() {
+  const poem = await fetchPoem();
+
+  if (!poem) {
+    return <div>No poem found</div>;
+  }
+
+  const formattedContent = poem.content.split(',').map((segment, index) => (
+    <span key={index}>
+      {segment}
+      {index < poem.content.split(',').length - 1 && <br />}
+    </span>
+  ));
+
   return (
     <main className="home flex min-h-screen flex-col items-center justify-between p-24 bg-white relative">
       <nav className="navbar navbar-expand-lg bg-body-tertiary position-absolute top-0 start-0 w-100">
@@ -19,7 +47,14 @@ export default function Home() {
         </div>
       </nav>
 
-      
+      <div className="home-top-poem poem w-100 bg-white min-h-[300px] flex flex-col justify-between">
+        <h2 className="text-2xl p-2">{poem.title}</h2>
+        <div className="w-100 flex justify-center">
+          <p className="main-content text-xl">{formattedContent}</p>
+        </div>
+        <p className="text-sm text-gray-500 p-2">By {poem.author} on {poem.date}</p>
+      </div>
+
     </main>
   );
 }
